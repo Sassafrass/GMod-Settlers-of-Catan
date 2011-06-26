ec15f0f779cba49667af756abe33d12af6bcc1b2
Frank Sasto <sastoman@gmail.com>
2011-06-26 17:48:09 -0600

    Fixed CEdge:GetAdjacentVertexs() Added CEdge:GetAdjacentEdges()

--- entities/entities/CatanTileEdge/shared.lua
+++ entities/entities/CatanTileEdge/shared.lua
@@ -3,31 +3,50 @@ ENT.Type = "anim"
 ENT.Base = "catantilesocket"
 
 --[[
-	Vertexs
+	Vertexs - 2 Vertexs per edge
 	]]
 
 function ENT:GetAdjacentVertexs()
 	
 	local vertexs = {}
-	vertexs[ 1 ] = self:GetVertex1()
-	vertexs[ 2 ] = self:GetVertex2()
-	
+	if( self:GetY() % 3 == 0 ) then
+		vertexs[ 1 ] = self:GetBoard():GetVertexAt( self:GetX(), self:GetY() * 2 / 3 - 1 )
+		vertexs[ 2 ] = self:GetBoard():GetVertexAt( self:GetX(), self:GetY() * 2 / 3 )
+	elseif( (self:GetY() - 1) % 3 == 0 ) then
+		vertexs[ 1 ] = self:GetBoard():GetVertexAt( self:GetX(), (self:GetY() - 1) * 2 / 3 )
+		vertexs[ 2 ] = self:GetBoard():GetVertexAt( self:GetX() + 1, (self:GetY() - 1) * 2 / 3 + 1 )
+	else
+		vertexs[ 1 ] = self:GetBoard():GetVertexAt( self:GetX(), (self:GetY() - 2) * 2 / 3 )
+		vertexs[ 2 ] = self:GetBoard():GetVertexAt( self:GetX(), (self:GetY() - 2) * 2 / 3 + 1 )
+	end
 	return vertexs
 	
 end
 
-function ENT:GetVertex1()
-	if( self:GetY() % 3 == 0 ) then
-		return self:GetBoard():GetVertexAt( self:GetX(), self:GetY() * 2 / 3 - 1 )
-	else
-		return self:GetBoard():GetVertexAt( self:GetX(), (self:GetY() - 1) * 2 / 3 )
-	end
-end
+--[[
+	Edges - 4 Edges per edge
+	]]
 
-function ENT:GetVertex2()
-	if( self:GetY() % 3 == 0 ) then
-		return self:GetBoard():GetVertexAt( self:GetX(), self:GetY() * 2 / 3 )
+function ENT:GetAdjacentEdges()
+	
+	local edges = {}
+	if( (self:GetY() - 1) % 3 == 0 ) then
+		edges[ 1 ] = self:GetBoard():GetEdgeAt( self:GetX(), self:GetY() + 1 )
+		edges[ 2 ] = self:GetBoard():GetEdgeAt( self:GetX(), self:GetY() - 1 )
+		edges[ 3 ] = self:GetBoard():GetEdgeAt( self:GetX() + 1, self:GetY() + 2 )
+		edges[ 4 ] = self:GetBoard():GetEdgeAt( self:GetX() + 1, self:GetY() + 1 )
+	elseif( self:GetY() % 3 == 0 ) then
+		edges[ 1 ] = self:GetBoard():GetEdgeAt( self:GetX() - 1, self:GetY() - 2 )
+		edges[ 2 ] = self:GetBoard():GetEdgeAt( self:GetX(), self:GetY() - 1 )
+		edges[ 3 ] = self:GetBoard():GetEdgeAt( self:GetX(), self:GetY() + 2 )
+		edges[ 4 ] = self:GetBoard():GetEdgeAt( self:GetX(), self:GetY() + 1 )
 	else
-		return self:GetBoard():GetVertexAt( self:GetX(), (self:GetY() - 1) * 2 / 3 + 1 )
+		edges[ 4 ] = self:GetBoard():GetEdgeAt( self:GetX(), self:GetY() + 1 )
+		edges[ 1 ] = self:GetBoard():GetEdgeAt( self:GetX() - 1, self:GetY() - 1 )
+		edges[ 2 ] = self:GetBoard():GetEdgeAt( self:GetX(), self:GetY() - 1 )
+		edges[ 3 ] = self:GetBoard():GetEdgeAt( self:GetX(), self:GetY() - 2 )
 	end
+	
+	return edges
+	
 end
--- entities/entities/CatanTileVertex/shared.lua
+++ entities/entities/CatanTileVertex/shared.lua
@@ -9,36 +9,18 @@ ENT.Base = "catantilesocket"
 function ENT:GetAdjacentVertexs()
 	
 	local vertexs = {}
-	vertexs[ 1 ] = self:GetVertex1()
-	vertexs[ 2 ] = self:GetVertex2()
-	vertexs[ 3 ] = self:GetVertex3()
+	if( self:GetY() % 2 == 0 ) then
+		vertexs[ 1 ] = self:GetBoard():GetVertexAt( self:GetX(), self:GetY() - 1 )
+		vertexs[ 2 ] = self:GetBoard():GetVertexAt( self:GetX(), self:GetY() + 1 )
+		vertexs[ 3 ] = self:GetBoard():GetVertexAt( self:GetX() + 1, self:GetY() + 1 )
+	else
+		vertexs[ 1 ] = self:GetBoard():GetVertexAt( self:GetX() - 1, self:GetY() - 1 )
+		vertexs[ 2 ] = self:GetBoard():GetVertexAt( self:GetX(), self:GetY() + 1 )
+		vertexs[ 3 ] = self:GetBoard():GetVertexAt( self:GetX(), self:GetY() - 1 )
+	end
 	
 	return vertexs
 	
-end
-
-function ENT:GetVertex1()
-	if( self:GetY() % 2 == 0 ) then
-		return self:GetBoard():GetVertexAt( self:GetX(), self:GetY() - 1 )
-	else
-		return self:GetBoard():GetVertexAt( self:GetX() - 1, self:GetY() - 1 )
-	end
-end
-
-function ENT:GetVertex2()
-	if( self:GetY() % 2 == 0 ) then
-		return self:GetBoard():GetVertexAt( self:GetX(), self:GetY() + 1 )
-	else
-		return self:GetBoard():GetVertexAt( self:GetX(), self:GetY() + 1 )
-	end
-end
-
-function ENT:GetVertex3()
-	if( self:GetY() % 2 == 0 ) then
-		return self:GetBoard():GetVertexAt( self:GetX() + 1, self:GetY() + 1 )
-	else
-		return self:GetBoard():GetVertexAt( self:GetX(), self:GetY() - 1 )
-	end
 end
 
 --[[
@@ -48,36 +30,18 @@ end
 function ENT:GetAdjacentEdges()
 	
 	local edges = {}
-	edges[ 1 ] = self:GetEdge1()
-	edges[ 2 ] = self:GetEdge2()
-	edges[ 3 ] = self:GetEdge3()
+	if( self:GetY() % 2 == 0 ) then
+		edges[ 1 ] = self:GetBoard():GetEdgeAt( self:GetX(), self:GetY() / 2 * 3 )
+		edges[ 2 ] = self:GetBoard():GetEdgeAt( self:GetX(), self:GetY() / 2 * 3 + 1 )
+		edges[ 3 ] = self:GetBoard():GetEdgeAt( self:GetX(), self:GetY() / 2 * 3 + 2 )
+	else
+		edges[ 1 ] = self:GetBoard():GetEdgeAt( self:GetX() - 1, (self:GetY() - 1) / 2 * 3 + 1 )
+		edges[ 2 ] = self:GetBoard():GetEdgeAt( self:GetX(), (self:GetY() - 1) / 2 * 3 + 2 )
+		edges[ 3 ] = self:GetBoard():GetEdgeAt( self:GetX(), (self:GetY() - 1) / 2 * 3 + 3 )
+	end
 	
 	return edges
 	
-end
-
-function ENT:GetEdge1()
-	if( self:GetY() % 2 == 0 ) then
-		return self:GetBoard():GetEdgeAt( self:GetX(), self:GetY() / 2 * 3 )
-	else
-		return self:GetBoard():GetEdgeAt( self:GetX() - 1, (self:GetY() - 1) / 2 * 3 + 1 )
-	end
-end
-
-function ENT:GetEdge2()
-	if( self:GetY() % 2 == 0 ) then
-		return self:GetBoard():GetEdgeAt( self:GetX(), self:GetY() / 2 * 3 + 1 )
-	else
-		return self:GetBoard():GetEdgeAt( self:GetX(), (self:GetY() - 1) / 2 * 3 + 2 )
-	end
-end
-
-function ENT:GetEdge3()
-	if( self:GetY() % 2 == 0 ) then
-		return self:GetBoard():GetEdgeAt( self:GetX(), self:GetY() / 2 * 3 + 2 )
-	else
-		return self:GetBoard():GetEdgeAt( self:GetX(), (self:GetY() - 1) / 2 * 3 + 3 )
-	end
 end
 
 --[[
@@ -87,34 +51,16 @@ end
 function ENT:GetAdjacentTiles()
 	
 	local tiles = {}
-	tiles[ 1 ] = self:GetTile1()
-	tiles[ 2 ] = self:GetTile2()
-	tiles[ 3 ] = self:GetTile3()
+	if( self:GetY() % 2 == 0 ) then
+		tiles[ 1 ] = self:GetBoard():GetTileAt( self:GetX(), self:GetY() / 2 )
+		tiles[ 2 ] = self:GetBoard():GetTileAt( self:GetX() + 1, self:GetY() / 2 )
+		tiles[ 3 ] = self:GetBoard():GetTileAt( self:GetX() + 1, self:GetY() / 2 + 1 )
+	else
+		tiles[ 1 ] = self:GetBoard():GetTileAt( self:GetX(), (self:GetY() - 1) / 2 + 1 )
+		tiles[ 2 ] = self:GetBoard():GetTileAt( self:GetX(), (self:GetY() - 1) / 2 )
+		tiles[ 3 ] = self:GetBoard():GetTileAt( self:GetX() + 1, (self:GetY() - 1) / 2 + 1 )
+	end
 	
 	return tiles
 	
-end
-
-function ENT:GetTile1()
-	if( self:GetY() % 2 == 0 ) then
-		return self:GetBoard():GetTileAt( self:GetX(), self:GetY() / 2 )
-	else
-		return self:GetBoard():GetTileAt( self:GetX(), (self:GetY() - 1) / 2 + 1 )
-	end
-end
-
-function ENT:GetTile2()
-	if( self:GetY() % 2 == 0 ) then
-		return self:GetBoard():GetTileAt( self:GetX() + 1, self:GetY() / 2 )
-	else
-		return self:GetBoard():GetTileAt( self:GetX(), (self:GetY() - 1) / 2 )
-	end
-end
-
-function ENT:GetTile3()
-	if( self:GetY() % 2 == 0 ) then
-		return self:GetBoard():GetTileAt( self:GetX() + 1, self:GetY() / 2 + 1 )
-	else
-		return self:GetBoard():GetTileAt( self:GetX() + 1, (self:GetY() - 1) / 2 + 1 )
-	end
 end
